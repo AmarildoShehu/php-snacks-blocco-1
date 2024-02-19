14ab6004a262990d6a3143247414484c352779eb
Amarildo <you@example.com>
2024-02-19 22:40:13 +0100

    created form control snack 2

---
+++ snack2/script/index.php
@@ -0,0 +1,17 @@
+
+<?php
+if(isset($_GET['name']) && isset($_GET['mail']) && isset($_GET['age'])) {
+    $name = $_GET['name'];
+    $mail = $_GET['mail'];
+    $age = $_GET['age'];
+
+    // Verifica delle condizioni richieste
+    if(strlen($name) > 3 && filter_var($mail, FILTER_VALIDATE_EMAIL) && is_numeric($age)) {
+        echo "Accesso riuscito";
+    } else {
+        echo "Accesso negato";
+    }
+} else {
+    echo "Parametri mancanti";
+}
+?>
